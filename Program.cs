14fa6ba3a32bdaffc546811250eb1f52c1c026d6
KonkolWSB <56751974+KonkolWSB@users.noreply.github.com>
2019-12-14 12:46:12 +0100

    Generate letter raport
    fixes #7

--- Program.cs
+++ Program.cs
@@ -38,8 +38,39 @@ namespace ConsoleApp1
                     {
 
                     }
+                    if (x == 2)
+                    {
+                        try
+                        {
 
+                            string text = System.IO.File.ReadAllText(@"5.txt");
+                            if (text != null)
+                            {
+                                if (text == null)
+                                {
+                                    Console.WriteLine("Brak pliku ");
+                                    break;
+                                }
+                                int count = 0;
+                                foreach (char y in text)
+                                {
+                                    string n = Convert.ToString(y);
+                                    if (n != "," && n != "." && n != ";" && n != "'" && n != "?" && n != "!" && n != "-" && n != ":")
+                                    {
+                                        count++;
+
+                                    }
+
+                                }
+                                Console.WriteLine("Ilość liter = " + count);
+                            }
+
+                        }
+                        catch { }
+
+                    }
                 }
+              
                 if (x == 3)
                 {
                     Console.Clear();
@@ -68,6 +99,42 @@ namespace ConsoleApp1
                     finally
                     {
 
+                    }
+
+                    if (x == 6)
+                    {
+                        Console.Clear();
+                        try
+                        {
+                            string text = System.IO.File.ReadAllText(@"5.txt");
+                            if (text != null)
+                            {
+
+                                int[] character = new int[(int)char.MaxValue];
+
+                                foreach (char tp in text)
+                                {
+
+                                    character[(int)tp]++;
+                                }
+
+
+                                for (int i = 0; i < (int)char.MaxValue; i++)
+                                {
+                                    if (character[i] > 0 && char.IsLetterOrDigit((char)i))
+                                    {
+                                        Console.WriteLine("{0},{1}", (char)i, character[i]);
+                                    }
+                                }
+
+
+                            }
+                        }
+
+                        catch (FileNotFoundException e)
+                        {
+                            Console.WriteLine("Błąd nie ma pliku");
+                        }
                     }
                     if (x == 8)
                     {
